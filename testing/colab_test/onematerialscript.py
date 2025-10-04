c21b7de7e973dde09c2a0e39414385da6119d5cd
khushipatel6 <khushi.patel6@mail.utoronto.ca>
2025-10-04 08:47:36 -0400

    adding some comments

--- testing/colab_test/onematerialscript.py
+++ testing/colab_test/onematerialscript.py
@@ -1,13 +1,14 @@
 import bpy
 
 #this script is used within blender to make all the materials of all the objects the same
+#to assume concrete, change name of material to "itu_concrete"
 
 # Clean up old materials
 for mat in bpy.data.materials:
     bpy.data.materials.remove(mat, do_unlink=True)
 
 # Create one material for the scene
-mat = bpy.data.materials.new(name="MyMaterial")
+mat = bpy.data.materials.new(name="itu_concrete")
 
 # Assign to all mesh objects
 for obj in bpy.data.objects:
