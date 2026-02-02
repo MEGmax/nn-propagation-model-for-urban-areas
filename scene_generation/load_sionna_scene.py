12312dbeb4c4e153bfe26e68c64943868d0d9340
khushi patel <khushipatel@khushis-MacBook-Air.local>
2026-02-02 18:45:56 -0500

    removing null value handling when creating sionna scenes as we are adding mask instead

--- scene_generation/load_sionna_scene.py
+++ scene_generation/load_sionna_scene.py
@@ -78,11 +78,11 @@ for i in range(len(list(Path(SCENE_DIR).iterdir()))):
     rm.show(metric="rss")
 
     # Handle -inf values before saving
-    rss_map = rm.rss
-    rss_map[rss_map == -np.inf] = 99999
+    # rss_map = rm.rss
+    #rss_map[rss_map == -np.inf] = 99999
 
     # set -inf values back to minimum for difference calculation
-    rss_map[rss_map == 99999] = rss_map.min()
+    #rss_map[rss_map == 99999] = rss_map.min()
 
     np.save(f"automated_scenes/scene{i}/rss_values{i}.npy", rm.rss)
 
